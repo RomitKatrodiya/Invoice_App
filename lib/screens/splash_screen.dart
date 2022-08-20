af5b551a49e1c1bde939277756dc3b73f3f89dd8
Romit Katrodiya <romitkatrodiya2308@gmail.com>
2022-08-20 13:01:38 +0530

    add Splash Screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -4,8 +4,20 @@ import 'package:invoice_app_new/styles/styles.dart';
 
 import 'home_page.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () => Get.off(() => HomePage()));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,19 +28,24 @@ class SplashScreen extends StatelessWidget {
         color: controller.themeColor.value.shade100,
         child: Column(
           children: [
-            Spacer(),
+            const Spacer(),
             Icon(
               Icons.assignment_outlined,
               size: 150,
               color: controller.themeColor.value,
             ),
-            Spacer(),
-            CircularProgressIndicator(),
-            SizedBox(
-              height: 30,
+            const Spacer(),
+            CircularProgressIndicator(color: controller.themeColor.value),
+            const SizedBox(height: 40),
+            Text(
+              "Invoice Generator",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: controller.themeColor.value,
+              ),
             ),
-            Text("Invoice Genretor"),
-            Spacer(),
+            const SizedBox(height: 40),
           ],
         ),
       ),
